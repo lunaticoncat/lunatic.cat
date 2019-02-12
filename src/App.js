4434e034e0d8b982b5025125957f642b7df11eab
Vlad Bokov <razum2um@mail.ru>
2019-02-12 14:10:01 +0700

    React router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
 import './App.scss';
 import Landing from './Landing';
 
 import dataEn from './data';
 import dataCat from './data.cat';
 
-const data = window.location.pathname === '/cat' ? dataCat : dataEn;
-
-const {
-  nav,
-  landing,
-  landingHeader,
-  footer,
-} = data;
-
-const landingProps = {...landing, header: landingHeader, nav, footer };
 
 class App extends Component {
+  landing(data) {
+    const {
+      nav,
+      landing,
+      landingHeader,
+      footer,
+    } = data;
+
+    const landingProps = {...landing, header: landingHeader, nav, footer };
+    return <Landing {...landingProps} />;
+  }
+
   render() {
     return (
-      // <div className="App">
-      //   <div className="App-label">Lunatic.cat</div>
-      // </div>
-      <Landing {...landingProps} />
+      <BrowserRouter basename={'/'}>
+        <Switch>
+          <Route path="/" exact render={props => (this.landing(dataEn))} />
+          <Route path="/cat" exact render={props => (this.landing(dataCat))} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
 import useScrollPosition from './../../hooks/useScrollPosition';
 
 export default ({ title, links }) => {
@@ -12,7 +14,7 @@ export default ({ title, links }) => {
                <a href='/' className="navbar__logo cat-font">{title}</a>
                <nav className="navbar__menu">
                  <ul>
-                   { links.map(e => <li key={e.link}><a href={e.link}>{e.text}</a></li>) }
+                   { links.map(e => <li key={e.link}><Link to={e.link}>{e.text}</Link></li>) }
                  </ul>
                </nav>
                <div className="navbar__menu-mob"><a href='#nav' id='toggle'><svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" className=""></path></svg></a></div>
