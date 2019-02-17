04bcb7994e3dbf384b79644bb4f52dc96e41a634
Vlad Bokov <razum2um@mail.ru>
2019-02-17 15:38:13 +0700

    External resolution for ffmpeg

--- bin/encode-x264.sh
+++ bin/encode-x264.sh
@@ -1,1 +1,3 @@
-ffmpeg -i $1 -c:v libx264 -profile:v baseline -level 3.0 -movflags +faststart -vf scale=600:-1 $2
+set -euox pipefail
+
+ffmpeg -i $1 -c:v libx264 -profile:v baseline -level 3.0 -movflags +faststart -vf scale=$3 -an $2
