785fb45b6e99d56e06634f15218fb9a5590363a1
Vlad Bokov <razum2um@mail.ru>
2019-02-18 08:06:12 +0700

    Autoplay with muted=muted

--- src/components/LandingSection/index.js
+++ src/components/LandingSection/index.js
@@ -9,7 +9,7 @@ export default ({ img, video, title, text }) => {
                <div className="expanded__inner">
                  <div className="expanded__media">
                    {img ? <Image src={img} className="expanded__image" /> : null }
-                   {video ? (<video autoPlay loop defaultmuted="true" playsInline onContextMenu={() => {}} preload="auto" className={`expanded__video ${video.vertical ? 'expanded__video--vertical' : ''}`} poster={video.poster}>
+                   {video ? (<video autoPlay loop defaultmuted="true" muted="muted" playsInline onContextMenu={() => {}} preload="auto" className={`expanded__video ${video.vertical ? 'expanded__video--vertical' : ''}`} poster={video.poster}>
                      {video.sources.map((source, idx) => <source src={source.src} type={source.type} key={idx} />)}
                    </video>): null }
                  </div>
